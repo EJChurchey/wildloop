c6b59cf2146d8671b6bbce7a1844bc16c68364e9
Your Name <everettchurchey@crbc.edu>
2015-03-31 10:48:15 -0400

    Initial Commit

---
+++ wildloops/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wildloops
+//
+//  Created by Ej Churchey on 3/31/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
